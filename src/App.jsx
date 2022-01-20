cfde6dd231840f0ecc3633a155c39cd6a7fae75e
Iuliia91 <you@example.com>
2022-01-20 23:58:31 +0200

    prepering finished

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Mainlayouts from 'Layouts/MainLayouts/Mainlayouts'
+
+class App extends React.Component {
+  render() {
+    return <Mainlayouts />
+  }
+}
+
+export default App
---
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MainLayouts = () => {
+  return <div>Hello World!</div>
+}
+
+export default MainLayouts
