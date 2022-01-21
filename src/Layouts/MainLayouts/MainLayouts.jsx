0930541e4f8947bd84043627e6369484594b2f28
Iuliia91 <you@example.com>
2022-01-21 18:15:30 +0200

    working with style

--- src/Layouts/MainLayouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-
+import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const MainLayouts = () => {
   return (
     <div className="layot">
       <div className="main">
         <div className="main_content">
-          <button className="button button__singIn">SingIn</button>
+          <div className="button_option">
+            <button className="button button__singIn">
+              <div>
+                <FontAwesomeIcon icon={faHome} />
+                SingIn
+              </div>
+            </button>
+
+            <button className="button button__registration">
+              <div>
+                <FontAwesomeIcon icon={faUser} />
+                Registr{' '}
+              </div>
+            </button>
+          </div>
+
           <div className="main_text">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
             tempora eos eum enim molestias! Doloremque soluta quo quisquam
           </div>
-          <button className="button">Click me</button>
+          <button className="button">Read more</button>
         </div>
       </div>
     </div>
