cabf55478b8cc8d7bf3a7dfea1eb49127e7d0ed0
Iuliia91 <you@example.com>
2022-01-27 22:37:33 +0200

    fdgbfd

--- src/Layouts/MainLayouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -1,48 +1,20 @@
 import React, { useState } from 'react'
-import ButtonOptions from 'Components/ButtonOptions'
-import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import OpenWindowSingIn from 'Components/OpenWindowSingIn'
-import { proposalPlugins } from '@babel/preset-env/data/shipped-proposals'
-import Navigation from 'Components/Navigation'
+
+import InitialScenes from 'Scenes/InitialScenes'
+import styled from 'styled-components'
+
+const StyledMainLayouts = styled.div`
+  .layot {
+    background-image: url(/src/Helpers/imiges/gfb.jpg);
+  }
+`
 
 const MainLayouts = () => {
   const [modalContext, setModalContext] = useState('')
 
-  const changeName = (context) => {
-    setModalContext(context)
-  }
-
   return (
     <div className="layot">
-      {!!modalContext && <div className="modalwindow">{modalContext}</div>}
-      <Navigation />
-      <div className="main">
-        <OpenWindowSingIn changeName={changeName} />
-
-        <div className="main_content">
-          <div className="main_text">
-            <div className="button_option">
-              <ButtonOptions
-                changeName={changeName}
-                className="button button__singIn"
-                textInsideButton={'Sing In'}
-                iconOptions={<FontAwesomeIcon icon={faHome} />}
-              />
-              <ButtonOptions
-                className="button button__registration"
-                textInsideButton={'Registr'}
-                iconOptions={<FontAwesomeIcon icon={faUser} />}
-              />
-            </div>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
-              tempora eos eum enim molestias! Doloremque soluta quo quisquam
-            </p>
-          </div>
-          <button className="button">Read more</button>
-        </div>
-      </div>
+      <InitialScenes />
     </div>
   )
 }
--- src/Scenes/InitialScenes.jsx
+++ src/Scenes/InitialScenes.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { faUser, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ButtonOptions from 'Components/ButtonOptions'
 
 const StyledInitialScenes = styled.div`
   background-image: url(/src/Helpers/imiges/gfb.jpg);
@@ -8,32 +11,77 @@ const StyledInitialScenes = styled.div`
   height: 100%;
   width: 100%;
   background-color: rgb(223, 230, 236);
+
+  .main {
+    max-width: 1200px;
+    position: fixed;
+    display: block;
+    right: 0;
+    width: 60%;
+  }
+
+  .main_content {
+    width: 100%;
+    position: absolute;
+    padding-top: 250px;
+    text-align: center;
+  }
+
+  .button {
+    border: none;
+  }
+
+  .button:hover {
+    background-color: rgb(199, 211, 222);
+  }
+  .button_option {
+    display: flex;
+    position: absolute;
+    top: 0;
+    right: 0;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .button__singIn,
+  .button__registration {
+    padding: 10px;
+    margin-right: 20px;
+  }
+
+  .button__read-more {
+    padding: 10px;
+    margin-right: 20px;
+  }
 `
 
 const InitialScenes = () => {
   return (
-    <div className="main_content">
-      <div className="main_text">
-        <div className="button_option">
-          <ButtonOptions
-            changeName={changeName}
-            className="button button__singIn"
-            textInsideButton={'Sing In'}
-            iconOptions={<FontAwesomeIcon icon={faHome} />}
-          />
-          <ButtonOptions
-            className="button button__registration"
-            textInsideButton={'Registr'}
-            iconOptions={<FontAwesomeIcon icon={faUser} />}
-          />
+    <StyledInitialScenes>
+      <div className="main">
+        <div className="main_content">
+          <div className="main_text">
+            <div className="button_option">
+              <ButtonOptions
+                className="button button__singIn"
+                textInsideButton={'Sing In'}
+                iconOptions={<FontAwesomeIcon icon={faHome} />}
+              />
+              <ButtonOptions
+                className="button button__registration"
+                textInsideButton={'Registr'}
+                iconOptions={<FontAwesomeIcon icon={faUser} />}
+              />
+            </div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos
+              tempora eos eum enim molestias! Doloremque soluta quo quisquam
+            </p>
+          </div>
+          <button className="button button__read-more">Read more</button>
         </div>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos tempora
-          eos eum enim molestias! Doloremque soluta quo quisquam
-        </p>
       </div>
-      <button className="button">Read more</button>
-    </div>
+    </StyledInitialScenes>
   )
 }
 
