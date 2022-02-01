5a0d149a3f0d02da145423229ab5230b98afaf03
Iuliia91 <you@example.com>
2022-02-01 23:48:34 +0200

    hgjnh

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[contenthash].js',
     clean: true,
+    publicPath: '/',
   },
   devtool: 'source-map',
   devServer: {
