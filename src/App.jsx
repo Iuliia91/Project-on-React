c2fd3d457818e6cc9695c7723590b7b8cc4fe8d4
Iuliia91 <you@example.com>
2022-02-03 13:54:30 +0200

    jnj

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Provider } from 'react-redux'
+import store from 'store/initStore'
 import GlobalModalProvider from 'HOC/GlobalModalProvider'
 import { BrowserRouter } from 'react-router-dom'
 import RootRouter from 'Route/RootRouter'
@@ -8,12 +9,14 @@ import GlobalStyledProvide from 'HOC/GlobalStyled'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <GlobalStyledProvide />
-      <GlobalModalProvider>
-        <RootRouter />
-      </GlobalModalProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyledProvide />
+        <GlobalModalProvider>
+          <RootRouter />
+        </GlobalModalProvider>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
---
+++ src/store/actionTypes.js
@@ -0,0 +1,3 @@
+export const CARD_LIST_ACTIONS = {
+  add: 'addCard',
+}
