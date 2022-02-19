042d6e39ffd383364aa367f6d1acdf9cd9528797
Iuliia91 <you@example.com>
2022-02-19 12:54:00 +0200

    jk,j

--- src/Scenes/ListofProducts.jsx
+++ src/Scenes/ListofProducts.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import recipeCard from 'store/actions/recipeCard'
+import axios from 'axios'
 
 const StyledListOfProducts = styled.div``
 
-const ListOfProducts = () => {
+const ListOfProducts = (props) => {
+  const options = {
+    method: 'GET',
+    url: 'http://localhost:3000/recipes',
+  }
+
   return (
     <StyledListOfProducts>
-      <div>gfbhfghnjgnjm</div>
+      <div></div>
+
+      <div>
+        <button
+          onClick={() => {
+            axios
+              .request(options)
+              .then((response) => {
+                console.log(response.data)
+                /*dispatch(recipeCard(response.data))*/
+              })
+              .catch((error) => console.log(error))
+          }}
+        >
+          Get App
+        </button>
+      </div>
     </StyledListOfProducts>
   )
 }
